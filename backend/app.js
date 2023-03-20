200b4c7a6c09e1c6bf094c337ab8bb857a1dfe4d
etrigo1 <bjorn.sjostedt@gmail.com>
2023-03-20 13:11:34 +0100

    connect app to db

--- backend/app.js
+++ backend/app.js
@@ -3,11 +3,25 @@ const path = require('path')
 const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 
 const indexRouter = require('./routes/index')
 const usersRouter = require('./routes/users')
 
 const app = express()
+
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect(process.env.MONGODB_URI, { useUnifiedTopology: true })
+  .then(client => {
+    console.log('App is connected to db')
+    const db = client.db('bjorn_sjostedt')
+    app.locals.db = db
+  })
+  .catch(error => {
+    console.log('Could not connect to database', error)
+    process.exit(1)
+  })
 
 app.use(cors())
 app.use(logger('dev'))
