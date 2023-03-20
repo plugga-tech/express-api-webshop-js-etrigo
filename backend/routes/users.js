422a05873ee0ad9e403fc24ce5fae76bdd02cb18
etrigo1 <bjorn.sjostedt@gmail.com>
2023-03-20 13:42:01 +0100

    sceleton routes added

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET users listing. */
+/* Get all users excluding password. */
 router.get('/', (req, res) => {
+  res.send('respond with a resource')
+})
+
+/* Get specific user object. */
+router.post('/', (req, res) => {
+  res.send('respond with a resource')
+})
+
+/* Create new user */
+router.post('/add', (req, res) => {
+  res.send('respond with a resource')
+})
+
+/* Login user */
+router.post('/login', (req, res) => {
   res.send('respond with a resource')
 })
 
