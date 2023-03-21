83acabcb4549fd070df7821ce49d6d91c66921a4
etrigo1 <bjorn.sjostedt@gmail.com>
2023-03-21 13:03:26 +0100

    Some reworking and endpoint fixes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,42 +1,70 @@
 const express = require('express')
 const router = express.Router()
 const CryptoJS = require('crypto-js')
+const { ObjectId } = require('mongodb')
 
-/* Get all users excluding password. */
+/* Get all users excluding password. - DONE */
 router.get('/users', (req, res) => {
-  res.send('respond with a resource')
+  req.app.locals.db
+    .collection('users')
+    .find()
+    .toArray()
+    .then(users => {
+      if (users) {
+        const censoredUsers = users.map(user => {
+          const { password, ...theRest } = user
+          return theRest
+        })
+        res.status(200).json(censoredUsers)
+      } else {
+        res.status(400).json({ error: 'No users found' })
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error)
+    })
 })
 
-/* Get specific user object. */
+/* Get specific user object. - DONE */
 router.post('/users', (req, res) => {
-  res.send('respond with a resource')
-})
-
-/* Create new user */
-router.post('/users/add', (req, res) => {
-  if (req.headers.authorization === process.env.API_KEY) {
-    const newUser = { ...req.body }
-    const encryptedPassword = CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.PASSWORD_SALT
-    ).toString()
-    newUser.password = encryptedPassword
-
+  try {
+    const id = new ObjectId(req.body.id)
     req.app.locals.db
       .collection('users')
-      .insertOne(newUser)
+      .findOne({ _id: id })
       .then(result => {
-        console.log('New user added to collection', result)
-        res.status(200).json(result)
+        if (result) {
+          res.status(200).json(result)
+        } else {
+          res.status(400).json({ error: 'No user found' })
+        }
       })
       .catch(error => {
-        console.log('Could not add user to collection', error)
-        res.status(500).json({ error: error.details[0].message })
+        res.status(500).json(error)
       })
-  } else {
-    console.log('Not a autorizeds request, bad api key')
-    res.status(401).json({ message: 'Not authorized, bad api key' })
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid id format' })
   }
+})
+
+/* Create new user - DONE */
+router.post('/users/add', (req, res) => {
+  const newUser = { ...req.body }
+  const encryptedPassword = CryptoJS.AES.encrypt(
+    req.body.password,
+    process.env.PASSWORD_SALT
+  ).toString()
+  newUser.password = encryptedPassword
+
+  req.app.locals.db
+    .collection('users')
+    .insertOne(newUser)
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(error => {
+      res.status(500).json({ error: error.details[0].message })
+    })
 })
 
 /* Login user */
